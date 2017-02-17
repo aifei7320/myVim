f0aeb92b83ee31067b2219b8842285be618692ef
aifei7320 <zhengxiaofeng333@163.com>
2017-02-17 08:27:33 +0800

    clone and execute build.sh,then make

---
+++ build.sh
@@ -0,0 +1,10 @@
+./configure --with-features=huge \
+            --enable-multibyte \
+            --enable-rubyinterp=yes \
+            --enable-pythoninterp=yes \
+            --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
+            --with-python3-config-dir=/usr/lib/python3/config-3.5m-x86_64-linux-gnu \
+            --enable-python3interp=yes \
+            --enable-perlinterp=yes \
+            --enable-luainterp \
+            --enable-gui=gtk2 --enable-cscope --prefix=/usr
